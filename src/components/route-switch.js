461cfbe7fad89e8b6340f852a6679ca0edc99eb9
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-17 13:40:04 -0500

    added routeswitch structure

--- src/components/route-switch.js
+++ src/components/route-switch.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ShoppingCart from "./shopping-cart";
 import HomePage from "./home-page";
 import Store from "./store";
+import "../styles/route-switch.css";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="heading">
+          <h1>Running Shoes</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/store">Store</Link>
+              </li>
+              <li>
+                <button>Cart</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </BrowserRouter>
+      <ShoppingCart />
+    </>
   );
 };
 
