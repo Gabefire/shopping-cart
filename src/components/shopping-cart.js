48d0b83f523958bacca1ac50f16e8f2ec94de59a
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-17 23:36:58 -0500

    set it up to display cart

--- src/components/shopping-cart.js
+++ src/components/shopping-cart.js
@@ -1,9 +1,17 @@
 import "../styles/shopping-cart.css";
 
-const ShoppingCart = ({ store, cart }) => {
+const ShoppingCart = ({ cart }) => {
   return (
     <div className="shopping-cart">
       <h2>Shopping Cart</h2>
+      {cart.map((element) => {
+        console.log(element);
+        return (
+          <div className="item">
+            <img className="cart-img" src={element.shoe} alt={element.name} />
+          </div>
+        );
+      })}
     </div>
   );
 };
