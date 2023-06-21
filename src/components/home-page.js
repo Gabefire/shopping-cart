60d43e4dc45a13f6c0de6c0f1f605726524f973a
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-20 22:19:20 -0500

    added background video

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,5 +1,25 @@
+import "../styles/home-page.css";
+import running from "../assets/running.mp4";
+import runningPic from "../assets/poster.jpg";
+
 const HomePage = () => {
-  return <div className="HomePage">You ARE a Runner</div>;
+  return (
+    <>
+      <video id="video-home-page" autoPlay loop muted>
+        <source src={running} type="video/mp4" />
+        {runningPic}
+      </video>
+      <div className="home-page">
+        <div className="home-page-quote">You ARE a Runner</div>
+        <p className="home-page-p">
+          freestar freestar Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Sed nunc neque, fringilla eget aliquet quis, luctus at nunc.
+          Nullam venenatis mauris ultricies augue blandit consectetur.{" "}
+        </p>
+        <button>Shop Now</button>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
